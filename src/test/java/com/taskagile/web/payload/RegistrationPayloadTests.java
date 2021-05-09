5ffc687a866a4d916138abcc42237b25e8082801
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-10 00:04:03 +0900

    validate without annotation

---
+++ src/test/java/com/taskagile/web/payload/RegistrationPayloadTests.java
@@ -0,0 +1,31 @@
+package com.taskagile.web.payload;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RegistrationPayloadTests {
+  private Validator validator;
+
+  @BeforeEach
+  public void setup() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  public void validate_blankPayload_shouldFail() {
+    RegistrationPayload payload = new RegistrationPayload();
+    Set<ConstraintViolation<RegistrationPayload>> violations = validator.validate(payload);
+    Assertions.assertEquals(3, violations.size());
+  }
+}
