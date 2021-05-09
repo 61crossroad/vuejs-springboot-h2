5a2f5d4b9e05afd2dd7e9458ee51185d520a59a0
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-09 16:53:54 +0900

    fix jest test, all passed

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    devServer: {
-        port: 3000,
-        proxy: {
-            '/api/*': {
-                target: 'http://localhost:8080'
-            }
-        }
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8080'
+      }
     }
+  }
 }
