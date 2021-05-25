05d15789ac18ed90ab5f3bcb67ef036f88991ea3
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-26 00:44:09 +0900

    implement PasswordEncryptorDelegate

--- src/main/java/com/taskagile/domain/common/mail/DefaultMailManager.java
+++ src/main/java/com/taskagile/domain/common/mail/DefaultMailManager.java
@@ -13,7 +13,6 @@ public class DefaultMailManager implements MailManager {
   @Override
   public void send(String emailAddress, String subject, String template, MessageVariable... variables) {
     // TODO Auto-generated method stub
-    log.info("[send email]\nemail: {}\nsubject: {}\ntemplate: {}\n[variables]\n", emailAddress, subject, template);
-    Arrays.stream(variables).forEach(v -> log.info(v.getKey(), v.getValue().getClass().getName()));
+    log.info("[send email] {} -> {}", emailAddress, subject);
   }
 }
--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,13 +1,18 @@
 package com.taskagile.domain.common.security;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
 
+  private final PasswordEncoder passwordEncoder;
+
   @Override
   public String encrypt(String rawPassword) {
-    // TODO Auto-generated method stub
-    return rawPassword;
+    return passwordEncoder.encode(rawPassword);
   }
 }
