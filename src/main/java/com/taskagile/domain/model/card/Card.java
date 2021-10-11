cad00c447462b533995e52ecd1bcd621ebef0d41
김승환 at Home <61crossroad@gmail.com>
2021-10-12 01:36:00 +0900

    feat: implement Card (cont..)

--- src/main/java/com/taskagile/domain/model/card/Card.java
+++ src/main/java/com/taskagile/domain/model/card/Card.java
@@ -1,6 +1,7 @@
 package com.taskagile.domain.model.card;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,9 +12,13 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.taskagile.domain.common.model.AbstractBaseId;
+import com.taskagile.domain.model.cardList.CardListId;
+import com.taskagile.domain.model.user.UserId;
+
 @Entity
 @Table(name = "card")
-public class Card {
+public class Card extends AbstractBaseId {
 
   private static final long serialVersionUID = 6030626206663838257L;
 
@@ -43,7 +48,79 @@ public class Card {
   @Column(name = "created_date", nullable = false)
   private Date createdDate;
 
-  public static Card create(CardListId carListId, UserId userId, String title, int position) {
-
+  public static Card create(CardListId cardListId, UserId userId, String title, int position) {
+    Card card = new Card();
+    card.cardListId = cardListId.value();
+    card.userId = userId.value();
+    card.title = title;
+    card.description = "";
+    card.position = position;
+    card.archived = false;
+    card.createdDate = new Date();
+    return card;
   }
+
+  public CardId getId() {
+    return new CardId(id);
+  }
+
+  public CardListId getCardListId() {
+    return new CardListId(cardListId);
+  }
+
+  public UserId getUserId() {
+    return new UserId(userId);
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public boolean isArchived() {
+    return archived;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Card)) return false;
+    Card card = (Card) o;
+    return cardListId == card.cardListId &&
+      userId == card.userId &&
+      position == card.position &&
+      archived == card.archived &&
+      Objects.equals(title, card.title);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cardListId, userId, title, position, archived);
+  }
+
+  @Override
+  public String toString() {
+    return "Card(" +
+      "id=" + id +
+      ", cardListId=" + cardListId +
+      ", userId=" + userId +
+      ", title='" + title + '\'' +
+      ", description='" + description + '\'' +
+      ", position=" + position +
+      ", archived=" + archived +
+      ", createdDate=" + createdDate +
+      "}";
+  }
+
 }
--- src/main/java/com/taskagile/domain/model/card/CardPosition.java
+++ src/main/java/com/taskagile/domain/model/card/CardPosition.java
@@ -1,5 +1,7 @@
 package com.taskagile.domain.model.card;
 
+import com.taskagile.domain.model.cardList.CardListId;
+
 public class CardPosition {
 
   private long cardListId;
@@ -9,4 +11,25 @@ public class CardPosition {
   public CardListId getCardListId() {
     return new CardListId(cardListId);
   }
+
+  public CardId getCardId() {
+    return new CardId(cardId);
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void setCardListId(long cardListId) {
+    this.cardListId = cardListId;
+  }
+
+  public void setCardId(long cardId) {
+    this.cardId = cardId;
+  }
+
+  public void setPosition(int position) {
+    this.position = position;
+  }
+
 }
